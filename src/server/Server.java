45fbd291c032f25e0403d8623ac9a7d26be045df
stephen ryan <stephen.ryan2@student.ncirl.ie>
2013-04-09 00:23:54 +0100

    updating client code

--- src/server/Server.java
+++ src/server/Server.java
@@ -34,7 +34,9 @@ public class Server {
     	// register the messageserver
     	MessageServerImpl msServer = new MessageServerImpl(rootPOA);
     	// register the service servant
+    	msServer.setORB(orb);
     	ServiceServant handlerServant = new ServiceServant(rootPOA);
+    	handlerServant.setORB(orb);
     	// register the listener for call back
     	ListenerImpl listen = new ListenerImpl();
         rootPOA.activate_object(listen);
@@ -42,7 +44,7 @@ public class Server {
         msServer.register(reference);
     	MessageServerPOATie tie = new MessageServerPOATie(msServer, rootPOA);
     	org.omg.CORBA.Object ref = rootPOA.servant_to_reference(tie);
-    	servicePOATie serviceTie = new servicePOATie(handlerServant);
+    	servicePOATie serviceTie = new servicePOATie(handlerServant, rootPOA);
     	org.omg.CORBA.Object ref2 = rootPOA.servant_to_reference(serviceTie);
     	//MessageServer href2 = tie._this(orb);
     	//service href = serviceTie._this(orb);
@@ -54,8 +56,8 @@ public class Server {
     	NameComponent path[] = namingref.to_name(messageServer);
     	NameComponent path2[] = namingref2.to_name(serverServant);
     	
-    	namingref.rebind(path, ref);
-    	namingref2.rebind(path2, ref2);
+    	namingref.rebind(path, ref2);
+    	namingref2.rebind(path2, ref);
      
       System.out.println("Object ref is " + orb.object_to_string(objref));
 
@@ -66,6 +68,8 @@ public class Server {
       //msServer.startReadThread();
       System.out.println("Server is ready...");
       orb.run();
+      System.out.println("orb running");
+      
     } catch (Exception e) {
       System.err.println("ERROR: " + e);
       e.printStackTrace(System.out);
